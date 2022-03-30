addf0764f8b3fcc62e853e008cf31f316ef59c2d
Regivaldo Sousa <professor.regi@colegiologo.com.br>
2022-03-30 16:03:32 -0300

    editando matrizes

---
+++ arrays com matrizes.R
@@ -0,0 +1,9 @@
+#arrays
+a<- matrix(1:20,4,5)
+b<- matrix(21:40,5,4)
+
+ conjunto<- array( c("por","mat","geo","fis","qui","bio"),dim = c(6,6,2))
+
+  array(b, dim = c(4,5,2)) # ou// array(b, dim = c(4,5,))
+conjunto
+  
